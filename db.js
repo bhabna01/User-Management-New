b662b7cee51e42137ef04d1030c14a2859c48c88
bhabna01 <abierhoque01@gmail.com>
2025-01-24 16:17:11 +0600

    first commit

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+require("dotenv").config();
+const db = mysql.createConnection({
+  host: process.env.DB_HOST, // XAMPP MySQL default host
+  user: process.env.DB_USER,      // Default XAMPP MySQL username
+  password: process.env.DB_PASSWORD,      // Default XAMPP MySQL password is blank
+  database: process.env.DB_NAME, // The database you created
+  port:process.env.PORT
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+  } else {
+    console.log("Connected to the MySQL database.");
+  }
+});
+
+module.exports = db;
